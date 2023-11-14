77e504ba7b325917ffd1bbdb1568b29ab248b91c
J Read <j@8bit.coffee>
2023-11-14 14:20:46 -0600

    complete with stretch

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,53 @@
-import React, { useState } from 'react';
+import { useCallback, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const airlineList = useSelector(store => store.airlineList);
+  const [airlineName, setAirlineName] = useState("");
+  const [numPlanes, setNumPlanes] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let action={
+      type: "ADD_AIRLINE",
+      payload: {
+        name: airlineName,
+        planes: numPlanes
+      }
+    };
+    dispatch(action);
+    setAirlineName("");
+    setNumPlanes("");
+  }
 
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      <form onSubmit={handleSubmit}>
+        Name: <input value={airlineName} placeholder='Airline Name' onChange={(e)=>setAirlineName(e.target.value)}/>
+        <br></br>
+        Number: <input value={numPlanes} type="number" placeholder='Number of planes' onChange={(e)=>setNumPlanes(e.target.value)}/>
+        <br></br>
+        <button>Add Airline</button>
+      </form>
+      <br></br>
+      <hr></hr>
+      <br></br>
+      <table>
+        <thead>
+          <tr>
+            <th>Airline</th>
+            <th># of planes</th>
+          </tr>
+        </thead>
+        {airlineList.map((airline,index) => 
+          <tr key={index}>
+            <td>{airline.name}</td>
+            <td>{airline.planes}</td>
+          </tr>
+        )}
+      </table>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,19 +3,33 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
 /** TODO: import REDUX **/
-
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
 
 /** TODO: Add REDUCERS */
-
+const airlineList = (state = [], action) => {
+    if (action.type === "ADD_AIRLINE"){
+        return [...state, action.payload];
+    }
+    return state;
+}
 
 /** TODO: Create store */
-
+const store = createStore(
+    combineReducers({
+        airlineList
+    }),
+    applyMiddleware(logger)
+);
 
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
